7203b49d496f2f792329c7293022a4877da3c709
SmasherK <ksikalan@gmail.com>
2023-07-18 14:44:57 +0200

    first edits

--- code/main.js
+++ code/main.js
@@ -1,30 +1,30 @@
 // Eighton-Lee Paulse - Code to push user debts into an array NB: This is not able to be tested at this moment so it will be commented
 
-// let debts = [];
-// const nameInput = document.querySelector("#name");
-// const priceInput = document.querySelector("#price");
-// const add = document.querySelector("#btn");
+let debts = [];
+const nameInput = document.querySelector("#name");
+const priceInput = document.querySelector("#price");
+const add = document.querySelector("#btn");
 
-// add.addEventListener("click", (e) => {
-//   console.log(e.target);
-//   try {
-//     if (nameInput && priceInput) {
-//       let newObject = {
-//         name: nameInput.value,
-//         price: priceInput.value,
-//       };
+add.addEventListener("click", (e) => {
+  console.log(e.target);
+  try {
+    if (nameInput && priceInput) {
+      let newObject = {
+        name: nameInput.value,
+        price: priceInput.value,
+      };
 
-//       debts.push(newObject);
-//       localStorage.setItem("debts", JSON.stringify(debts));
-//     } else {
-//       alert("ERROR");
-//     }
-//   } catch (e) {
-//     alert("ERROR");
-//   }
+      debts.push(newObject);
+      localStorage.setItem("debts", JSON.stringify(debts));
+    } else {
+      alert("ERROR");
+    }
+  } catch (e) {
+    alert("ERROR");
+  }
 
-//   setTimeout(() => {
-//     console.log("REACHED");
-//     window.history.back();
-//   }, 2000);
-// });
+  setTimeout(() => {
+    console.log("REACHED");
+    window.history.back();
+  }, 2000);
+});
